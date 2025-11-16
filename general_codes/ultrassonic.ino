dc8c49047e7d28873ac446e5d1f194a389341a97
Vitor Augusto Tibério <142597619+VitorTiberio@users.noreply.github.com>
2025-11-16 20:32:13 -0300

    Add ultrasonic sensor code in ultrassonic.ino

---
+++ general_codes/ultrassonic.ino
@@ -0,0 +1,34 @@
+// Developed by: Vitor Augusto Tibério  
+
+// Undergraduate student at University of São Paulo (USP) - Electrical Engineering // 
+
+// Definindo os pinos // 
+
+# define ECHO 18
+# define TRIGGER 5
+# define LED 21
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  pinMode(TRIGGER, OUTPUT);
+  pinMode(ECHO, INPUT);
+  pinMode(LED, OUTPUT);
+}
+
+void loop() {
+  // Gera o pulso do ultrassônico //
+  digitalWrite(TRIGGER, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIGGER, HIGH);
+  delayMicroseconds(10); // Pulso de 10 microssegundos
+  digitalWrite(TRIGGER, LOW);
+  // Calcula a distância do ultrassônico //
+  long duration = pulseIn(ECHO, HIGH);
+  float distanceCm = duration * 0.0343 / 2.0;
+  // Lê (serialmente) a leitura do sensor //
+  Serial.print("Distancia: ");
+  Serial.print(distanceCm);
+  Serial.println(" cm");
+  delay(500);
+}
